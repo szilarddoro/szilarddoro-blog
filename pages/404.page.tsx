e94593cfde7840018508c04d97954b12c7a2b581
Dóró Szilárd <szilard.doro@bishop.hu>
2022-01-08 19:41:11 +0100

    fixed a build issue

--- pages/404.page.tsx
+++ pages/404.page.tsx
@@ -2,10 +2,32 @@ import Link from 'next/link'
 import Heading from '../components/heading'
 import Layout from '../components/layout'
 import Paragraph from '../components/paragraph'
+import contentfulClient from '../lib/contentful-client'
+import type {
+  ConfigurationModel,
+  ConvertedConfiguration,
+} from '../types/configuration.types'
 
-export default function NotFound() {
+export type NotFoundPageProps = {
+  /**
+   * Site configuration.
+   */
+  siteConfiguration: ConvertedConfiguration
+}
+
+export default function NotFound({siteConfiguration}: NotFoundPageProps) {
+  if (!siteConfiguration) {
+    throw new Error(
+      `Site configuration is not available. Make sure you set up Contentful and your environment variables correctly.`,
+    )
+  }
+
   return (
-    <Layout seoProps={{noindex: true, nofollow: true}} className="grid gap-2">
+    <Layout
+      siteName={siteConfiguration.fields.title}
+      seoProps={{noindex: true, nofollow: true}}
+      className="grid gap-2"
+    >
       <Heading variant="h2" component="h1">
         A keresett oldal nem található
       </Heading>
@@ -18,4 +40,22 @@ export default function NotFound() {
       </Link>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const siteConfigurationId = process.env.SITE_CONFIGURATION_ID
+
+  let siteConfiguration: ConvertedConfiguration | null = null
+
+  if (siteConfigurationId) {
+    siteConfiguration = await contentfulClient.getEntry<ConfigurationModel>(
+      siteConfigurationId,
+    )
+  }
+
+  return {
+    props: {
+      siteConfiguration,
+    },
+  }
 }
