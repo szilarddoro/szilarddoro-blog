78f8499ceeee1d04a0cc11cf3aa552d7da38fe2e
Dóró Szilárd <szilard.doro@bishop.hu>
2022-01-15 15:17:28 +0100

    Fixed an issue where article info was displayed in a weird way on mobile devices

--- components/article-info/article-info.tsx
+++ components/article-info/article-info.tsx
@@ -40,62 +40,64 @@ export default function ArticleInfo<TBody>({
     >
       {children}
 
-      <div className="grid grid-flow-col place-content-start items-center gap-1">
+      <div className="grid grid-flow-row md:grid-flow-col gap-y-1 md:gap-y-0 place-content-start place-items-start items-center gap-x-1 text-sm sm:text-base">
         {!hideAuthor && (
-          <>
-            <div className="inline-grid grid-flow-col items-center gap-2">
-              <Image
-                src={post.fields.author.fields.image.relative_url}
-                alt={post.fields.author.fields.image.context.custom.alt}
-                width={32}
-                height={32}
-                className="rounded-full"
-                layout="fixed"
-                priority
-              />
+          <div className="inline-grid grid-flow-col items-center gap-2">
+            <Image
+              src={post.fields.author.fields.image.relative_url}
+              alt={post.fields.author.fields.image.context.custom.alt}
+              width={32}
+              height={32}
+              className="rounded-full"
+              layout="fixed"
+              priority
+            />
 
-              <span>{post.fields.author.fields.name}</span>
-            </div>
-
-            <span> · </span>
-          </>
+            <span>{post.fields.author.fields.name}</span>
+          </div>
         )}
 
-        <span>
-          {new Intl.DateTimeFormat(post.sys.locale).format(
-            new Date(post.sys.createdAt),
-          )}
-        </span>
+        {!hideAuthor && <span className="hidden md:inline"> · </span>}
 
-        <span> · </span>
+        <div className="grid grid-flow-col items-center gap-1">
+          <span>
+            {new Intl.DateTimeFormat(post.sys.locale).format(
+              new Date(post.sys.createdAt),
+            )}
+          </span>
 
-        <div className="grid grid-flow-col gap-0.5">
-          {post.fields.readingTime < 5 && <span>☕</span>}
-          {post.fields.readingTime >= 5 && post.fields.readingTime < 10 && (
-            <span>☕☕</span>
-          )}
-          {post.fields.readingTime >= 10 && <span>☕☕☕</span>}
-          <span>{post.fields.readingTime} perc</span>
-        </div>
+          <span> · </span>
 
-        <span> · </span>
+          <div className="flex flex-wrap items-center">
+            {post.fields.readingTime < 5 && <span className="mr-0.5">☕</span>}
+            {post.fields.readingTime >= 5 && post.fields.readingTime < 10 && (
+              <span className="mr-0.5">☕☕</span>
+            )}
+            {post.fields.readingTime >= 10 && (
+              <span className="mr-0.5">☕☕☕</span>
+            )}
+            <span>{post.fields.readingTime} perc</span>
+          </div>
 
-        <div className="inline-grid grid-flow-col gap-1.5">
-          {post.fields.tags.map(tag => (
-            <Link
-              href={`/categories/${tag.sys.id}`}
-              passHref
-              key={tag.sys.id}
-              prefetch={false}
-            >
-              <a className="group focus:outline-none">
-                <span className="text-xs inline-block">🔗</span>
-                <span className="group-hover:underline group-hover:text-emerald-500 group-active:text-emerald-600 group-focus-within:text-emerald-500 group-focus-visible:underline motion-safe:transition-colors ml-0.5">
-                  {tag.name}
-                </span>
-              </a>
-            </Link>
-          ))}
+          <span> · </span>
+
+          <div>
+            {post.fields.tags.map(tag => (
+              <Link
+                href={`/categories/${tag.sys.id}`}
+                passHref
+                key={tag.sys.id}
+                prefetch={false}
+              >
+                <a className="flex flex-wrap items-center group focus:outline-none">
+                  <span className="text-xs inline-block mr-0.5">🔗</span>
+                  <span className="group-hover:underline group-hover:text-emerald-500 group-active:text-emerald-600 group-focus-within:text-emerald-500 group-focus-visible:underline motion-safe:transition-colors ml-0.5">
+                    {tag.name}
+                  </span>
+                </a>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head />
 
-      <body className="font-body text-lg text-gray-900 dark:text-white dark:bg-gray-900">
+      <body className="font-body text-base text-gray-900 dark:text-white dark:bg-gray-900">
         <Main />
         <NextScript />
       </body>
